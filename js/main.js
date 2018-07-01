319ef25b13ac8d02e2d5fe751e02af349c90caae
Duli-mws <40149054+Duli-mws@users.noreply.github.com>
2018-07-01 18:01:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://free.currencyconverterapi.com';
-const LIST_OF_COUNTRIES = '/api/v5/countries';
+const URL = 'https://free.currencyconverterapi.com';
+const COUNTRIES = '/api/v5/countries';
 const CONVERT = `/api/v5/convert?q=`;
 
 const defaultCountry = '';
